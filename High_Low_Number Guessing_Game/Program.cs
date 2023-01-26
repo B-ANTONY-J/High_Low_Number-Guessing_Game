c118ba28a5b44780d3f03de557b4ad016f9bb094
B-ANTONY-J <antony.jones14@gmail.com>
2023-01-26 17:26:07 -0500

    Add project files.

---
+++ High_Low_Number Guessing_Game/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace High_Low_Number_Guessing_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next();
+            for (int j = 0; j < 10; j++)
+            {
+                Console.WriteLine(rnd.Next(10));
+            }
+        }
+    }
+}
