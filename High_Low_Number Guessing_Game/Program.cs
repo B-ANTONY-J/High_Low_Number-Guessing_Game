9b4de60ccb921e6c6ddf4237c886ef32317f7216
B-ANTONY-J <antony.jones14@gmail.com>
2023-01-29 21:21:13 -0500

    Code Updated to Reflect only Computer Generated Responses at this point with The For Loop and If Statements.  1/29

--- High_Low_Number Guessing_Game/Program.cs
+++ High_Low_Number Guessing_Game/Program.cs
@@ -4,11 +4,29 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("This Game Will start by giving the Computer 10 opportunities " +
+                "To Guess the Correct Number. You only Win when you can avoid being Greater " +
+                "than 80(Too High) or Avoid being 25 or below(Too Low)");
+
             Random rnd = new Random();
-            int num = rnd.Next();
-            for (int j = 0; j < 10; j++)
+            for (int j = 1; j < 10; j++)
             {
-                Console.WriteLine(rnd.Next(10));
+                int num = rnd.Next(100);
+                //int num = rnd.Next(1, 100);
+                //Console.WriteLine(rnd.Next(100));
+                Console.WriteLine(num);
+                if (num >= 80)
+                {
+                    Console.WriteLine("Computer You're too High!!  You Lose!");
+                }
+                if (num > 26) 
+                {
+                    Console.WriteLine("Computer you Figured it out! You WIN!!!");
+                }
+                if (num <= 25) 
+                {
+                    Console.WriteLine("Computer You're too Low! You Lose!!!");
+                }
             }
         }
     }
