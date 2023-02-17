eb7f392c404010f57b3b4f10673c4bae21490c2b
B-ANTONY-J <antony.jones14@gmail.com>
2023-02-17 02:22:51 -0500

    Changed code for the guessing game to reflect "MAGIC_NUMBER" and conditional statements for above/below computer guess

--- High_Low_Number Guessing_Game/Program.cs
+++ High_Low_Number Guessing_Game/Program.cs
@@ -3,6 +3,7 @@
     internal class Program
     {
         const int MAX_GUESSES = 10;
+        const int MAGIC_NUMBER = 5;
 
         static void Main(string[] args)
         {
@@ -17,7 +18,7 @@
             Console.WriteLine("The Computer will enter a number guess.");
             Console.WriteLine("(The computers answer will be Displayed Once the User enters the correct guess)");
             Console.WriteLine($"Your have {MAX_GUESSES} attempts to guess the secret number");
-            
+            //test functionality of code guess with Console.WriteLine(computerGuess);
 
             for (int j = 1; j <= MAX_GUESSES; ++j)
             {
@@ -27,53 +28,55 @@
 
                 Console.WriteLine($"{Environment.NewLine}");
 
-                if (userInput > computerGuess)
-                {
-                    Console.WriteLine("You're higher than the computers guess!!!");
-                }
-                else if (userInput < computerGuess)
-                {
-                    Console.WriteLine("You're lower than the computers guess!!!");
-                }
-                else if (userInput == computerGuess)
-                {
 
+                if (userInput == computerGuess)
+                {
                     Console.WriteLine("You've WON!!!! You guessed the correct number!!!");
                     Console.WriteLine($"\nYour correct guess of {userInput} WINS the Game!!!\n");
                     break;
                 }
-                if (j == MAX_GUESSES && userInput == (computerGuess - 5))
+                if (j == MAX_GUESSES && userInput < computerGuess)
                 {
-                    Console.WriteLine($"{Environment.NewLine}Mmm, Mmm, You're close! If the Guess is only 5 off...");
-                    Console.WriteLine(computerGuess);
-                    Console.WriteLine($"{Environment.NewLine}Less than 5 points - you're Almost good at this Game - keep trying!!!");
+                    Console.WriteLine($"{Environment.NewLine}Mmm, Mmm, Better luck Next Game...\nYour guess of {userInput} was {computerGuess - userInput} off from the Computer Guess of {computerGuess}.");
                     break;
-
-                }
-                if (j == MAX_GUESSES && computerGuess > userInput)
-                {
-                    Console.WriteLine($"{Environment.NewLine}Computers correct guess revealed below as...");
-                    Console.WriteLine(computerGuess);
-                    Console.WriteLine($"{Environment.NewLine}Your last guess of {userInput} was {computerGuess - userInput} numbers off from the correct guess of {computerGuess}, better luck Next Time!");
-                    break;
-
-                }
-                if (j == MAX_GUESSES && userInput == (computerGuess + 5))
-                {
-                    Console.WriteLine($"{Environment.NewLine}Mmm, Mmm, You're close! If the Guess is only 5 off...");
-                    Console.WriteLine(computerGuess);
-                    Console.WriteLine($"{Environment.NewLine}More than 5 points - You're Almost good at this Game - keep trying!!!");
-                    break;
-
                 }
                 if (j == MAX_GUESSES && userInput > computerGuess)
                 {
-                    Console.WriteLine($"{Environment.NewLine}Computers correct guess revealed below as...");
-                    Console.WriteLine(computerGuess);
-                    Console.WriteLine($"{Environment.NewLine}This time Your last guess of {userInput} was {userInput - computerGuess} numbers off from the correct guess of {computerGuess}, better luck Next Time!\n");
+                    Console.WriteLine($"{Environment.NewLine}Mmm, Mmm, Better luck Next Game...\nYour guess of {userInput} was {userInput - computerGuess} off from the Computer Guess of {computerGuess}.");
                     break;
-
                 }
+                if ((userInput > computerGuess) && (userInput - computerGuess) >= (2 * MAGIC_NUMBER))
+                {
+                    Console.WriteLine($"{Environment.NewLine}This is just terrible You're 10 or more numbers ABOVE than the computers guess!!!\nYou can do better than that - Try Again!!!");
+                    continue;
+                }
+                if (userInput > computerGuess && (userInput - computerGuess) >= MAGIC_NUMBER)
+                {
+                    Console.WriteLine($"{Environment.NewLine}Oook, now You're more than 5 numbers ABOVE than the computers guess!!!\nYou're Almost good at this Game - keep trying!!!!!!");
+                    continue;
+                }
+                if (userInput > computerGuess && (userInput - computerGuess) < MAGIC_NUMBER)
+                {
+                    Console.WriteLine($"SO CLOSE!!!! You're Less then 4 numbers above the Computers Guess");
+                    continue;
+                }
+                if (computerGuess > userInput && (computerGuess - userInput) >= (2 * MAGIC_NUMBER))
+                {
+                    Console.WriteLine("Well this is embarising you're more than the 10 points below the computers guess!!!\nKeep trying.\n");
+                    continue;
+                }
+                if (computerGuess > userInput && (computerGuess - userInput) >= MAGIC_NUMBER)
+                {
+                    Console.WriteLine("Oh no, You're more than the 5 points below the computers guess!!!\nYou're getting better at this Game - keep trying!!!!!!\n");
+                    continue;
+                }
+                if ((computerGuess - userInput) < MAGIC_NUMBER)
+                {
+                    Console.WriteLine($"{Environment.NewLine}Wow, You're less than 4 numbers BELOW the computers guess so just focus and try to guess higher!!!");
+                    continue;
+                }
+
+
             }
 
 
