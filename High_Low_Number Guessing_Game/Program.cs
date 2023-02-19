80f40e161cd1e77f4e3aba8b704662e63cb1ef8b
B-ANTONY-J <antony.jones14@gmail.com>
2023-02-19 16:11:02 -0500

    added "else if" conditions and removed "continue" statements.

--- High_Low_Number Guessing_Game/Program.cs
+++ High_Low_Number Guessing_Game/Program.cs
@@ -3,7 +3,7 @@
     internal class Program
     {
         const int MAX_GUESSES = 10;
-        const int MAGIC_NUMBER = 5;
+        const int WITHIN_FIVE = 5;
 
         static void Main(string[] args)
         {
@@ -18,15 +18,16 @@
             Console.WriteLine("The Computer will enter a number guess.");
             Console.WriteLine("(The computers answer will be Displayed Once the User enters the correct guess)");
             Console.WriteLine($"Your have {MAX_GUESSES} attempts to guess the secret number");
-            //test functionality of code guess with Console.WriteLine(computerGuess);
+            // test functionality of code guess with 
+            Console.WriteLine(computerGuess);
 
             for (int j = 1; j <= MAX_GUESSES; ++j)
             {
 
-                Console.WriteLine($"{Environment.NewLine} {Environment.NewLine}Please enter a number guess.");
+                Console.WriteLine($"\n\nPlease enter a number guess.");
                 int userInput = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine($"{Environment.NewLine}");
+                Console.WriteLine($"\n");
 
 
                 if (userInput == computerGuess)
@@ -35,45 +36,45 @@
                     Console.WriteLine($"\nYour correct guess of {userInput} WINS the Game!!!\n");
                     break;
                 }
-                if (j == MAX_GUESSES && userInput < computerGuess)
-                {
-                    Console.WriteLine($"{Environment.NewLine}Mmm, Mmm, Better luck Next Game...\nYour guess of {userInput} was {computerGuess - userInput} off from the Computer Guess of {computerGuess}.");
-                    break;
-                }
                 if (j == MAX_GUESSES && userInput > computerGuess)
                 {
-                    Console.WriteLine($"{Environment.NewLine}Mmm, Mmm, Better luck Next Game...\nYour guess of {userInput} was {userInput - computerGuess} off from the Computer Guess of {computerGuess}.");
+                    Console.WriteLine($"\nMmm, Mmm, Better luck Next Game...\nYour guess of {userInput} was {userInput - computerGuess} off from the Computer Guess of {computerGuess}.");
                     break;
                 }
-                if ((userInput > computerGuess) && (userInput - computerGuess) >= (2 * MAGIC_NUMBER))
+                if (j == MAX_GUESSES && userInput < computerGuess)
                 {
-                    Console.WriteLine($"{Environment.NewLine}This is just terrible You're 10 or more numbers ABOVE than the computers guess!!!\nYou can do better than that - Try Again!!!");
-                    continue;
+                    Console.WriteLine($"\nMmm, Mmm, Better luck Next Game...\nYour guess of {userInput} was {computerGuess - userInput} off from the Computer Guess of {computerGuess}.");
+                    break;
                 }
-                if (userInput > computerGuess && (userInput - computerGuess) >= MAGIC_NUMBER)
+                if ((userInput > computerGuess) && (userInput - computerGuess) >= (2 * WITHIN_FIVE))
                 {
-                    Console.WriteLine($"{Environment.NewLine}Oook, now You're more than 5 numbers ABOVE than the computers guess!!!\nYou're Almost good at this Game - keep trying!!!!!!");
-                    continue;
+                    Console.WriteLine($"\nThis is just terrible You're 10 or more numbers ABOVE than the computers guess!!!\nYou can do better than that - Try Again!!!");
+
                 }
-                if (userInput > computerGuess && (userInput - computerGuess) < MAGIC_NUMBER)
+                else if (userInput > computerGuess && (userInput - computerGuess) >= WITHIN_FIVE)
+                {
+                    Console.WriteLine($"\nOook, now You're more than {WITHIN_FIVE} numbers ABOVE than the computers guess!!!\nYou're Almost good at this Game - keep trying!!!!!!");
+
+                }
+                else if (userInput > computerGuess && (userInput - computerGuess) < WITHIN_FIVE)
                 {
                     Console.WriteLine($"SO CLOSE!!!! You're Less then 4 numbers above the Computers Guess");
-                    continue;
+
                 }
-                if (computerGuess > userInput && (computerGuess - userInput) >= (2 * MAGIC_NUMBER))
+                else if (computerGuess > userInput && (computerGuess - userInput) >= (2 * WITHIN_FIVE))
                 {
                     Console.WriteLine("Well this is embarising you're more than the 10 points below the computers guess!!!\nKeep trying.\n");
-                    continue;
+
                 }
-                if (computerGuess > userInput && (computerGuess - userInput) >= MAGIC_NUMBER)
+                else if (computerGuess > userInput && (computerGuess - userInput) >= WITHIN_FIVE)
                 {
-                    Console.WriteLine("Oh no, You're more than the 5 points below the computers guess!!!\nYou're getting better at this Game - keep trying!!!!!!\n");
-                    continue;
+                    Console.WriteLine($"Oh no, You're more than the {WITHIN_FIVE} points below the computers guess!!!\nYou're getting better at this Game - keep trying!!!!!!\n");
+
                 }
-                if ((computerGuess - userInput) < MAGIC_NUMBER)
+                else if ((computerGuess - userInput) < WITHIN_FIVE)
                 {
-                    Console.WriteLine($"{Environment.NewLine}Wow, You're less than 4 numbers BELOW the computers guess so just focus and try to guess higher!!!");
-                    continue;
+                    Console.WriteLine($"\nWow, You're less than 4 numbers BELOW the computers guess so just focus and try to guess higher!!!");
+
                 }
 
 
