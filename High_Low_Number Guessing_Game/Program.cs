ff8425231c56efb4d0586847c75f4efccfc27dc3
B-ANTONY-J <64221875+B-ANTONY-J@users.noreply.github.com>
2023-03-19 16:25:37 -0400

    Removed "namespace" and braketed code with  being replaced with the "using" statement.

--- High_Low_Number Guessing_Game/Program.cs
+++ High_Low_Number Guessing_Game/Program.cs
@@ -1,16 +1,8 @@
-﻿namespace High_Low_Number_Guessing_Game
-{
-    internal class Program
-    {
+﻿
+using High_Low_Number_Guessing_Game;
+//Method to call User Input Variables
+UIMethods.UserInput();
 
-        static void Main(string[] args)
-        {
-            //Method to call User Input Variables
-            UIMethods.UserInput();
- 
-            //Method to call Loop Iteration through conditional if/else statements
-            UIMethods.MaxGuessLoop();
-        }
-        
-    }
-}
+//Method to call Loop Iteration through conditional if/else statements
+UIMethods.MaxGuessLoop();
+
